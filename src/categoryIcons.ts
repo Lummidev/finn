9017b75832f16d07ed32417ef2ed2959710d37f4
Gabriel Valeriano <53843213+Lummidev@users.noreply.github.com>
2025-10-15 15:44:35 -0300

    fix: Generate icon names dynamically

--- src/categoryIcons.ts
+++ src/categoryIcons.ts
@@ -19,27 +19,33 @@ import {
   faUtensils,
   type IconDefinition,
 } from "@fortawesome/free-solid-svg-icons";
-
-export const categoryIcons: Record<
+const iconOptions: [IconDefinition, string][] = [
+  [faBagShopping, "Sacola"],
+  [faBook, "Livro"],
+  [faBurger, "Hambúrger"],
+  [faBus, "Ônibus"],
+  [faCar, "Carro"],
+  [faComputer, "Computador"],
+  [faDumbbell, "Haltere"],
+  [faFutbol, "Bola de Futebol"],
+  [faGamepad, "Controle de videogame"],
+  [faGift, "Presente"],
+  [faGraduationCap, "Chapéu de formatura"],
+  [faHouse, "Casa"],
+  [faMobileScreen, "Celular"],
+  [faPaw, "Pata"],
+  [faPills, "Pílulas"],
+  [faStore, "Loja"],
+  [faTag, "Etiqueta"],
+  [faUtensils, "Utensílios"],
+];
+const categoryIcons: Record<
   string,
   { icon: IconDefinition; displayName: string }
-> = {
-  tag: { icon: faTag, displayName: "Etiqueta" },
-  house: { icon: faHouse, displayName: "Casa" },
-  car: { icon: faCar, displayName: "Carro" },
-  utensils: { icon: faUtensils, displayName: "Utensílios" },
-  burger: { icon: faBurger, displayName: "Hambúrger" },
-  gamepad: { icon: faGamepad, displayName: "Controle de videogame" },
-  computer: { icon: faComputer, displayName: "Computador" },
-  mobile: { icon: faMobileScreen, displayName: "Celular" },
-  graduationCap: { icon: faGraduationCap, displayName: "Chapéu de formatura" },
-  book: { icon: faBook, displayName: "Livro" },
-  paw: { icon: faPaw, displayName: "Pata" },
-  bus: { icon: faBus, displayName: "Ônibus" },
-  bag: { icon: faBagShopping, displayName: "Sacola" },
-  dumbbell: { icon: faDumbbell, displayName: "Haltere" },
-  futbol: { icon: faFutbol, displayName: "Bola de Futebol" },
-  pills: { icon: faPills, displayName: "Pílulas" },
-  gift: { icon: faGift, displayName: "Presente" },
-  store: { icon: faStore, displayName: "Loja" },
-};
+> = {};
+iconOptions.forEach((iconOption) => {
+  const [icon, displayName] = iconOption;
+  categoryIcons[icon.iconName] = { icon, displayName };
+});
+
+export { categoryIcons };
