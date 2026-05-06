f352937e6235894a143915ce9db73ae311567406
Gabriel Valeriano <53843213+Lummidev@users.noreply.github.com>
2026-05-06 11:01:41 -0300

    feat: Add chat date separators

--- src/Pages/Chat/Chat.tsx
+++ src/Pages/Chat/Chat.tsx
@@ -1,13 +1,49 @@
-import { use } from "react";
+import { Fragment, use, useMemo } from "react";
 import { ChatBubble } from "../../Components/ChatBubble/ChatBubble";
 import "./Chat.css";
 import { MessageContext } from "../../Context/MessageContext";
 import { PageHeader } from "../../Components/PageHeader/PageHeader";
 import { faTrash } from "@fortawesome/free-solid-svg-icons";
 import { MessageRepository } from "../../Database/MessageRepository";
+import type { Message } from "../../Entities/Message";
+import dayjs from "dayjs";
 
 export const Chat = () => {
   const messageHistory = use(MessageContext);
+  const dateMessageGroups = useMemo(() => {
+    const messagesGroupedByDate: Record<string, Message[]> = {};
+    messageHistory.forEach((message) => {
+      const date = dayjs(message.createdAtTimestampMiliseconds).startOf("day");
+      const today = dayjs().startOf("day");
+      const diff = today.diff(date, "days");
+      const weekPlusAgo = diff > 6;
+      const beforeYesterday = diff > 1;
+      const yesterday = diff === 1;
+      const formattedDate = ((formattedDate) => {
+        return (
+          String(formattedDate[0]).toUpperCase() +
+          String(formattedDate).slice(1)
+        );
+      })(
+        weekPlusAgo
+          ? date.format("L")
+          : beforeYesterday
+            ? date.format("dddd")
+            : yesterday
+              ? "Ontem"
+              : "Hoje",
+      );
+      if (!messagesGroupedByDate[formattedDate]) {
+        messagesGroupedByDate[formattedDate] = [message];
+      } else {
+        messagesGroupedByDate[formattedDate] = [
+          ...messagesGroupedByDate[formattedDate],
+          message,
+        ];
+      }
+    });
+    return messagesGroupedByDate;
+  }, [messageHistory]);
   return (
     <div className={`chat`}>
       <PageHeader
@@ -27,9 +63,17 @@ export const Chat = () => {
         ]}
       />
       <div className="chat__box">
-        {messageHistory.map((message) => (
-          <ChatBubble key={message.id} message={message} />
-        ))}
+        {Object.keys(dateMessageGroups).map((date) => {
+          const messages = dateMessageGroups[date];
+          return (
+            <Fragment key={date}>
+              {messages.map((message) => (
+                <ChatBubble key={message.id} message={message} />
+              ))}
+              <div className="chat__date-separator">{date}</div>
+            </Fragment>
+          );
+        })}
       </div>
     </div>
   );
